29498f645c79b5fd49a44722e1a2dccbde3e914a
Khall <ibrahimkhall212@gmail.com>
2019-05-31 12:02:05 +0100

    add sql dumper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use App\Http\View\Composers\ConfigComposer;
 use App\Models\Agent;
 use App\Observers\AgentObserver;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -19,15 +22,32 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Schema::defaultStringLength(191);
+
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
      * Bootstrap any application services.
      *
+     * @param UrlGenerator $url
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
+
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+
+        if($this->app->environment() === 'local') {
+            DB::listen(function(QueryExecuted $sql)
+            {
+                file_put_contents('php://stdout', "\e[34m{$sql->sql}\t\e[37m" . json_encode($sql->bindings) . "\t\e[32m{$sql->time}ms\e[0m\n");
+            });
+        }
+
         View::composer('*', ConfigComposer::class);
 
         Agent::observe(AgentObserver::class);
